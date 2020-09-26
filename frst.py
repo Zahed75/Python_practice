6d48f4952ced17660d6349aedc12ac98458f9ea0
Md.Zahed Hasan <56452604+Zahed75@users.noreply.github.com>
2020-09-27 00:55:22 +0600

    multiple argument

--- frst.py
+++ frst.py
@@ -1,1 +1,12 @@
 print("Hello Fake","Dragon","DIM",10,3,sep="+") #seprate the value
+
+print("My Bengal of gold",end="+") #end e code merge kora
+print("i hate you")
+
+print("My Bengal of gold",end="\n\n") # n for new line
+print("i hate you")
+
+
+# ==============note================
+
+
---
+++ print_multipleargument.py
@@ -0,0 +1,19 @@
+#print multiple argument
+#pass value as a perameter
+
+a = "i"
+b ="love"
+c ="you"
+ 
+print("hey",a,b,c)
+
+#print Strings format
+
+#sequential formating pass value
+a="I"
+b="You"
+c="Me"
+
+print("{} love {}".format(a,b))
+print("{} love {}".format(b,c))
+
