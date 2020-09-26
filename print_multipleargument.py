80b46e67b3d8263c9d1202dbc9b3230ec756e03b
Md.Zahed Hasan <56452604+Zahed75@users.noreply.github.com>
2020-09-27 01:06:36 +0600

    foramting print value

--- print_multipleargument.py
+++ print_multipleargument.py
@@ -17,3 +17,11 @@ c="Me"
 print("{} love {}".format(a,b))
 print("{} love {}".format(b,c))
 
+#==========Formating the number#
+
+a="You"
+b="kick"
+c="me"
+
+print("{} love {}".format(a,b))
+print("{1} hate {0}".format(c,b)) #format means pass value change by indexing
