604811139fecff51c57a3d72a9471e300f0d9a94
Md.Zahed Hasan <56452604+Zahed75@users.noreply.github.com>
2020-09-30 03:06:22 +0600

    strings

---
+++ strings.py
@@ -0,0 +1,18 @@
+#=====Assign a strings into a variabole
+
+
+print("helllo")
+
+print(type(print))
+
+
+#Assign a strings into a value
+ 
+
+a="amar sonaar gamla ami tomay valobashi"
+
+print(a)
+
+b = """amar sonar bangla ami tomay valobashi chirodin tomar aksah tomar batash"""
+
+print(b)
