dd430bfd5d0074b299f99c770ead8713d1c32eed
Md.Zahed Hasan <56452604+Zahed75@users.noreply.github.com>
2020-09-30 03:29:07 +0600

    operators update

--- operators.py
+++ operators.py
@@ -11,32 +11,54 @@
 # Membership operators
 # Bitwise operators
 
-a = 34
-b =545
-sum= a+b
-print(sum)
+# a = 34
+# b =545
+# sum= a+b
+# print(sum)
 
 
-a=43
-b =545
-sub= a-b
-print(sub)
+# a=43
+# b =545
+# sub= a-b
+# print(sub)
 
 
-a=43
-b =545
-mul= a*b
-print(mul)
+# a=43
+# b =545
+# mul= a*b
+# print(mul)
 
-a=43
-b =545
-div= a/b
-print(div)
+# a=43
+# b =545
+# div= a/b
+# print(div)
 
 #floor division
 
-x=5
-y=2
+# x=5
+# y=2
 
-floor= x%y
-print(floor) #vagsesh dekhabae
+# floor= x%y
+# print(floor) #vagsesh dekhabae
+
+#assignment operator
+
+# X =5
+# X+=5
+
+# print(X)
+
+# Python Comparison Operators
+# Comparison operators are used to compare two values:
+
+X=5
+Y=3
+
+print(X==Y)
+
+x = 5
+y = 3
+
+print(x != y)
+
+# returns True because 5 is not equal to 3
