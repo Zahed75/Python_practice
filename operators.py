4b91171649a57de42ae3fd43768f0fe661941a8d
Md.Zahed Hasan <56452604+Zahed75@users.noreply.github.com>
2020-09-30 03:20:48 +0600

    operators and boolean

---
+++ operators.py
@@ -0,0 +1,42 @@
+# Python Operators
+# Operators are used to perform operations on variables and values.
+
+# Python divides the operators in the following groups:
+
+# Arithmetic operators
+# Assignment operators
+# Comparison operators
+# Logical operators
+# Identity operators
+# Membership operators
+# Bitwise operators
+
+a = 34
+b =545
+sum= a+b
+print(sum)
+
+
+a=43
+b =545
+sub= a-b
+print(sub)
+
+
+a=43
+b =545
+mul= a*b
+print(mul)
+
+a=43
+b =545
+div= a/b
+print(div)
+
+#floor division
+
+x=5
+y=2
+
+floor= x%y
+print(floor) #vagsesh dekhabae
---
+++ pythonbooleans.py
@@ -0,0 +1,26 @@
+#python booleans practice
+#Booleans represent one of two values: True or False.
+
+print(10>9)
+print(8<3)
+print(10==6)
+
+#Print a message based on whether the condition is True or False:
+
+a =400
+b = 200
+if b>a:
+    print("b is a greater then a")
+
+else:
+    print("b is not greater then a")
+    
+#Example2
+a= 30
+c = 40
+
+if a>c:
+    print("C is greater then a")
+
+else:
+    print("C is not greater then a")
