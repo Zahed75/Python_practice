3355bcb86ee7b99f61e71d4361d425fc3cb5a761
Md.Zahed Hasan <56452604+Zahed75@users.noreply.github.com>
2020-09-30 03:00:04 +0600

    data type

---
+++ datatype.py
@@ -0,0 +1,31 @@
+#=====python Data type
+
+X = "hello"
+print(type(X))
+
+M = ("Cherry","Mitul","Bangladesh","Hello Heda") # first bracket diye tuple hoy
+ 
+print(type(M))
+
+
+N={"amar","sonar","bangla","hedar","gamla"} # set hoy Second bracket diye
+print(type(N))
+
+
+L=["hello","naveen","do you","hate me"] # List hoy Third bracket diye
+
+print(type(L))
+
+X = frozenset({"apple","hello","life","suck"})
+
+print(type(X))
+
+Z=range(6)
+
+print(Z)
+
+print(type(Z))
+
+
+P = 345643+2j
+print(type(P))
