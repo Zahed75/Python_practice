0a6937e4758c5ee70436c1f20eae3d26894cfc94
Md.Zahed Hasan <56452604+Zahed75@users.noreply.github.com>
2020-10-02 04:34:38 +0600

    quiz test for list

---
+++ list_quiz.py
@@ -0,0 +1,11 @@
+# print the second value
+fruits = ["apple", "mango", "litchi"]
+print(fruits[1])
+
+
+# change the value from apple to the litchi
+
+fruits = ["apple", "mango", "litchi"]
+
+fruits[0]= "litchi"
+print(fruits)
