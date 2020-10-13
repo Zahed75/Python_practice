126bfd77352a93323d11e7e7ebf0808e64ec4013
Md.Zahed Hasan <56452604+Zahed75@users.noreply.github.com>
2020-10-13 12:51:43 +0600

    last list update

---
+++ list2.py
@@ -0,0 +1,52 @@
+name = ("Zahed", "Hasan", "Morsheda", "neela", "nodi")
+
+print(name[1])
+
+print(len(name))
+
+print(name[-3])
+
+fruits = ("Mango", "stawbeery", "Litchi", "Guava", "Banana")
+
+print(fruits[2:4])
+
+print(fruits[2:4])
+
+college = ("DC", "Daffodil", "NDC", "Holy Cross", "UIU")
+for x in college:
+    print(x)
+
+
+name = ("zahed","zahedh","promi","neela","rayhan")
+if "zahed" in name:
+    print("yes we got the culprit")
+
+
+foods =["rice","pizza","chicken"]
+foods.append("oil")
+print(foods)
+
+kichuri =["rice","dal","chicken","slat"]
+kichuri.append("doi")
+print(kichuri)
+
+cake =["egg","suger","oil","falvour","baking powder","foodcolor"]
+cake.remove("foodcolor")
+print(cake)
+
+
+cake =["egg","suger","oil","falvour","baking powder","foodcolor"]
+cake.pop(1)
+print(cake)
+
+juice =["mango","banana","strabwerry","litchi"]
+del juice[1]
+print(juice)
+
+love = ["friend","best friend","janina","bal","heda"]
+
+del love
+
+result = ["34","345","53","3423","767","23","234"]
+result.clear()
+print(result)
