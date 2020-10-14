c5eee7310e41a1afc7b76725a3718c62f6e7e411
Md.Zahed Hasan <56452604+Zahed75@users.noreply.github.com>
2020-10-15 03:02:20 +0600

    vowel consonant find

---
+++ vowel_consonant.py
@@ -0,0 +1,6 @@
+letter = input("please enter your word:")
+
+if letter== 'a' or letter == 'b' or letter == 'e' or letter == 'i' or letter == 'o' or letter == 'u':
+    print("vowel")
+else:
+    print("consanant")
